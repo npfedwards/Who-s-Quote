1abf0884a972a906ddd213e9bdbad5d3f66530f4
Nathan Edwards <npfedwards@gmail.com>
2012-02-20 22:21:06 +0000

    Switched to a simpler idea.
    Now the app is based around the idea of people testing their knowledge
    of quotes. And the gamifying of this with fb/twitter.

--- newquote.php
+++ newquote.php
@@ -13,20 +13,15 @@
 			".stripslashes($row['Quote'])."
 		</blockquote>";
 		$query="SELECT * FROM authors WHERE QuoteID='$id'";
-		$result=mysql_query($query) or die(mysql_error());
 		while($row=mysql_fetch_assoc($result)){
-			$query="SELECT * FROM concurrance WHERE AuthorID='".$row['AuthorID']."'";
-			$r=mysql_query($query) or die(mysql_error());
 			$concurrances=mysql_num_rows($r);
 			$out=$out. "<div class='quoteauthor'>
-				".stripslashes($row['Author'])."
 				<span class='concurrance' id='con".$row['AuthorID']."'>";
 					if($concurrances!=0){
 						$out=$out. $concurrances;
 					}
 				$out=$out. "</span><a class='iconcur'><img src='img/accept.png' alt='tick'></a>
 				</div>";
-		}
 		$out=$out. "<div class='quoteauthor'>
 				  <input type='text' name='author' id='author' value='Who said/wrote this?' class='hintTextbox' onkeydown=\"if (event.keyCode == 13) AddAuthor('".$id."', this.value)\">
 				  <span id='response'></span>
