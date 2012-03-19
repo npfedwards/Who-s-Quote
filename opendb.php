f41436fac56e7e5516216a567a0b6f93e15316fe
Richard Brunt <r.brunt@gmail.com>
2012-03-19 22:05:51 +0000

    Force tableprefix to end with an underscore

--- opendb.php
+++ opendb.php
@@ -8,12 +8,19 @@
 	//Set the name of your database:
 	$dbname = 'wq';
 	//Set a table prefix for Who's Quotes - this allows multiple installations to be in the same database.  Useful if you only have access to one database (on shared hosting):
+	//It's best to use an underscore afterwards (e.g. 'wq_')
 	$tableprefix = '';
 	/*	
 		=================
 		End Customization Options
 		=================
 	*/
+	//Forcing underscore afterwards:
+	if ($tableprefix!='') {
+		if (substr($tableprefix, -1)!='_') {
+			$tableprefix=$tableprefix.'_';
+		}	
+	}
 	$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
 	$dbc = mysql_select_db($dbname) or die("Failed to connect");
 ?>
