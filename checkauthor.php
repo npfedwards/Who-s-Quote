7982978cd088194abe0e2c6bba2188319f652b0d
Richard Brunt <r.brunt@gmail.com>
2012-03-19 21:19:16 +0000

    continued work on tableprefix

--- checkauthor.php
+++ checkauthor.php
@@ -1,6 +1,5 @@
 <?php
 	include 'opendb.php'; //Open the Database
-	
 	//GET author and quoteID
 	$author=mysql_real_escape_string(htmlentities($_GET['a']));
 	$q=mysql_real_escape_string(htmlentities($_GET['q']));
@@ -10,7 +9,7 @@
 	$ip=mysql_real_escape_string($_SERVER['REMOTE_ADDR']);
 	
 	//Get user from table
-	$query="SELECT * FROM ".$dbprefix."users WHERE UserString='$user' AND IP='$ip'";
+	$query="SELECT * FROM ".$tableprefix."users WHERE UserString='$user' AND IP='$ip'";
 	$result=mysql_query($query) or die(mysql_error());
 	
 	if(mysql_num_rows($result)==1){ //if there is a user
@@ -21,46 +20,46 @@
 	}else{ //if there isn't a user (or if the IP address has changed)
 	
 		$user=sha1(rand()); //create a random hash
-		$query="SELECT * FROM ".$dbprefix."users WHERE UserString='$user'";
+		$query="SELECT * FROM ".$tableprefix."users WHERE UserString='$user'";
 		$result=mysql_query($query) or die(mysql_error());
 		
 		while(mysql_num_rows($result)==1){ //if we have a clash
 			$user=sha1(rand()); //create a random hash
-			$query="SELECT * FROM ".$dbprefix."users WHERE UserString='$user'";
+			$query="SELECT * FROM ".$tableprefix."users WHERE UserString='$user'";
 			$result=mysql_query($query) or die(mysql_error());
 		}
 		setcookie('user', $user, time()+86400*365);
 		//Insert new user data
-		$query="INSERT INTO ".$dbprefix."users (UserString, IP) VALUES ('$user','$ip')";
+		$query="INSERT INTO ".$tableprefix."users (UserString, IP) VALUES ('$user','$ip')";
 		mysql_query($query) or die(mysql_error());
 		$userid=mysql_insert_id();
 	}
 	
 	//Does the Author name match the DB?
-	$query="SELECT * FROM ".$dbprefix."quotes LEFT JOIN ".$dbprefix."authors ON ".$dbprefix."quotes.AuthorID=authors.AuthorID WHERE QuoteID='$q' AND ".$dbprefix."authors.AuthorID='$author' LIMIT 0,1";
+	$query="SELECT * FROM ".$tableprefix."quotes LEFT JOIN ".$tableprefix."authors ON ".$tableprefix."quotes.AuthorID=".$tableprefix."authors.AuthorID WHERE QuoteID='$q' AND ".$tableprefix."authors.AuthorID='$author' LIMIT 0,1";
 	$result=mysql_query($query) or die(mysql_error());
 	if(mysql_num_rows($result)==1){
 		//Database insert for correctness
-		$query="INSERT INTO ".$dbprefix."answers (UserID, QuoteID, Timestamp, Correct) VALUES ('$userid', '$q', '".time()."', '1')";
+		$query="INSERT INTO ".$tableprefix."answers (UserID, QuoteID, Timestamp, Correct) VALUES ('$userid', '$q', '".time()."', '1')";
 		mysql_query($query) or die(mysql_error());
 		
-		$query="SELECT * FROM ".$dbprefix."answers WHERE UserID='$userid'";
+		$query="SELECT * FROM ".$tableprefix."answers WHERE UserID='$userid'";
 		$result=mysql_query($query) or die(mysql_error());
 		$quotes=mysql_num_rows($result);
-		$query="SELECT * FROM ".$dbprefix."answers WHERE UserID='$userid' AND Correct='1'";
+		$query="SELECT * FROM ".$tableprefix."answers WHERE UserID='$userid' AND Correct='1'";
 		$result=mysql_query($query) or die(mysql_error());
 		$correct=mysql_num_rows($result);
 		//Send back some text...
 		echo "Correct! So far you have answered ".$correct."/".$quotes." correctly";
 	}else{
 		//Database insert for wrongness
-		$query="INSERT INTO ".$dbprefix."answers (UserID, QuoteID, Timestamp, Correct) VALUES ('$userid', '$q', '".time()."', '0')";
+		$query="INSERT INTO ".$tableprefix."answers (UserID, QuoteID, Timestamp, Correct) VALUES ('$userid', '$q', '".time()."', '0')";
 		mysql_query($query) or die(mysql_error());
 		
-		$query="SELECT * FROM ".$dbprefix."answers WHERE UserID='$userid'";
+		$query="SELECT * FROM ".$tableprefix."answers WHERE UserID='$userid'";
 		$result=mysql_query($query) or die(mysql_error());
 		$quotes=mysql_num_rows($result);
-		$query="SELECT * FROM ".$dbprefix."answers WHERE UserID='$userid' AND Correct='1'";
+		$query="SELECT * FROM ".$tableprefix."answers WHERE UserID='$userid' AND Correct='1'";
 		$result=mysql_query($query) or die(mysql_error());
 		$correct=mysql_num_rows($result);
 		//Send back some text...
--- opendb.php
+++ opendb.php
@@ -4,6 +4,6 @@
 	$dbpass = '';
 	$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
 	$dbname = 'wq';
-	$dbprefix = '';
+	$tableprefix = 'test_';
 	$dbc = mysql_select_db($dbname) or die("Failed to connect");
 ?>
